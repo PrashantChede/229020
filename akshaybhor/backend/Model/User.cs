589de5e6cb9d864720c6fd27024b3b85bb520017
PrashantChede <chedeprashant2022@gmail.com>
2023-01-17 15:14:09 +0530

    done

---
+++ akshaybhor/backend/Model/User.cs
@@ -0,0 +1,17 @@
+
+namespace UserManagementApi.Model;
+
+public class User
+{
+    public int userid { get; set; }
+
+
+    public string username { get; set; } 
+
+
+    public string course { get; set; } 
+
+
+    public string purchasedate { get; set; }
+}
+
